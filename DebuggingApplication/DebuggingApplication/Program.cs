6dcded650fd40798c9b1c2f83683828a09b6d4e8
Akash-Rana-25 <akashrana.work.gmail.com>
2023-04-19 14:08:01 +0530

    added try parse

--- DebuggingApplication/DebuggingApplication/Program.cs
+++ DebuggingApplication/DebuggingApplication/Program.cs
@@ -4,16 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Number:c");
             string userResponse = Console.ReadLine();
-
-            if (Convert.ToBoolean(int.Parse(userResponse)))
+            int userIntResponse;
+            if (int.TryParse(userResponse, out userIntResponse))
             {
-
-                Console.WriteLine("The entered value is a integer");
-
+                Console.WriteLine($"The entered value is integer: {userIntResponse}");
+            }
+            else
+            {
+                Console.WriteLine("The entered value is invalid, only integers are allowed");
             }
 
-
+            Console.ReadKey();
         }
     }
 }
